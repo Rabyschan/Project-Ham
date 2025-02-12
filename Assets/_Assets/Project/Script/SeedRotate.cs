cfa2ebaad0dc33d8d3e864bfd939fa3a98f8ab55
Rabyschan <dhkffkq25@gmail.com>
2025-02-12 10:15:37 +0900

    v.0.1.3 SYJ 레벨디자인1

---
+++ Assets/_Assets/Project/Script/SeedRotate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedRotate : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 450f;
+    [SerializeField] GameObject seed;
+
+    Vector3 _origin;
+
+    public bool _isEating = false;
+
+    private void Awake()
+    {
+        _origin = transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y += rotationSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, _origin.z);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isEating = true;
+            seed.SetActive(false);
+        }   
+    }
+}
